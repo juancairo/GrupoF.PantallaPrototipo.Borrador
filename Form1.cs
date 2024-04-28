bf7c068eac8fd6a86ef81dd0db4650692045741b
Juan Cruz <Juan Cruz@DESKTOP-5DJBF15>
2024-04-28 08:16:23 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace GrupoF.PantallaPrototipo.Borrador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
